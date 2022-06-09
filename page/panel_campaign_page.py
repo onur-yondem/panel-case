42a898a45378a3ca507b0c13e0a649567875f51e
Onur Can Yöndem <99740071+onur-yondem@users.noreply.github.com>
2022-06-10 01:56:09 +0300

    Update panel_campaign_page.py

--- page/panel_campaign_page.py
+++ page/panel_campaign_page.py
@@ -37,6 +37,18 @@ class PanelCampaignPage(BaseClass):
     RULES_ROUTER = (By.CLASS_NAME, "in-steps-wrapper__steps")
     IFRAME = (By.ID, "iframe-edit")
     ADD_GOAL_BUTTON = (By.ID, "add-new-goal")
+    WEEKEND_BUTTON = (By.CLASS_NAME, "weekend")
+    DUPLICATE_BUTTON = (By.ID, "duplicate")
+    ACCORDION_MENU = (By.CLASS_NAME, "in-accordion-wrapper__text")
+    DISPLAY_SELECTED_DAYS_BUTTON = (By.CSS_SELECTOR, "input[value='on-day']")
+    MONDAY_BUTTON = (By.CSS_SELECTOR, "label[for='Monday']")
+    TUESDAY_BUTTON = (By.CSS_SELECTOR, "label[for='Tuesday']")
+    THURSDAY_BUTTON = (By.CSS_SELECTOR, "label[for='Thursday']")
+    SUNDAY_BUTTON = (By.CSS_SELECTOR, "label[for='Sunday']")
+    DISPLAY_END_DATE_INPUT = (By.ID, "drop-down-search")
+    DISPLAY_END_DATE_CLOCK = (By.CLASS_NAME, "option__6")
+    PRIORITY_INPUT = (By.ID, "priority")
+    PRIORITY_TWO_BUTTON = (By.CLASS_NAME, "priority-2")
 
     def __init__(self, driver):
         self.driver = driver
@@ -48,33 +60,69 @@ class PanelCampaignPage(BaseClass):
         Create new campaign and edit settings
 
         """
+
+    def segment_settings(self):
         self.presence_for_element(self.SAVE_BUTTON).click()
+
+    def rule_settings(self):
+        time.sleep(5)
         self.move_to_element(self.PAGE_RULES_BUTTON)
         self.presence_for_element(self.PAGE_RULES_DROP_DOWN).click()
         self.presence_for_element(self.PAGE_TYPE_BUTTON).click()
         data.rule = self.presence_for_element(self.PAGE_RULES_DROP_DOWN).text
         data.page_type = self.presence_for_element(self.PAGE_TYPE_INPUT).text
         self.presence_for_element(self.SAVE_BUTTON).click()
+
+    def desing_settings(self):
         self.presence_for_element(self.ADD_NEW_VARIANT_BUTTON).click()
-        time.sleep(10)
-        self.move_to_element(self.FIRST_TEMPLATE_OVERLAY)
-        self.presence_for_element(self.FIRST_TEMPLATE_SELECT_BUTTON).click()
+        time.sleep(20)
+        self.move_to_element(self.FIRST_TEMPLATE_OVERLAY, False)
+        self.clickable_for_element(self.FIRST_TEMPLATE_SELECT_BUTTON).click()
         self.clickable_for_element(self.INFORMATION_OK_BUTTON).click()
         self.driver.switch_to.frame("iframe-edit")
         self.presence_for_element(self.IFRAME_HEADER).click()
         self.driver.switch_to.default_content()
         self.presence_for_element(self.INSERT_AFTER_BUTTON).click()
+        time.sleep(3)
         self.move_to_element(self.CAMPAIGN_SAVE_BUTTON)
-        data.variant_name = self.presence_for_element(self.VARIANT_NAME_INPUT).get_attribute("id")
+        self.presence_for_element(self.DUPLICATE_BUTTON)
         self.presence_for_element(self.SAVE_BUTTON).click()
+
+    def goal_settings(self):
         self.presence_for_element(self.ADD_GOAL_BUTTON)
         self.presence_for_element(self.SAVE_BUTTON).click()
+
+    def language_settings(self):
         self.presence_for_element(self.LANGUAGE_DROP_DOWN).click()
         self.presence_for_element(self.ALL_LANGUAGES_BUTTON).click()
+
+    def activation_time_settings(self):
         self.presence_for_all_elements(self.END_DATE)[1].click()
         self.presence_for_element(self.YEAR_UP_BUTTON).click()
         self.presence_for_element(self.YEAR_UP_BUTTON).clear()
         self.send_keys(self.YEAR_UP_BUTTON, "2023")
+        self.presence_for_all_elements(self.WEEKEND_BUTTON)[5].click()
+
+    def display_settings(self):
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        self.presence_for_element(self.ACCORDION_MENU).click()
+        self.driver.execute_script("arguments[0].click()", self.presence_for_element(self.DISPLAY_SELECTED_DAYS_BUTTON))
+        self.presence_for_element(self.MONDAY_BUTTON).click()
+        self.presence_for_element(self.TUESDAY_BUTTON).click()
+        self.presence_for_element(self.THURSDAY_BUTTON).click()
+        self.presence_for_element(self.SUNDAY_BUTTON).click()
+        self.presence_for_all_elements(self.DISPLAY_END_DATE_INPUT)[3].click()
+        self.presence_for_all_elements(self.DISPLAY_END_DATE_CLOCK)[5].click()
+        self.presence_for_element(self.ACCORDION_MENU).click()
+        time.sleep(1)
+
+    def advanced_settings(self):
+        self.presence_for_all_elements(self.ACCORDION_MENU)[1].click()
+        self.presence_for_element(self.PRIORITY_INPUT).click()
+        self.presence_for_element(self.PRIORITY_TWO_BUTTON).click()
+        self.presence_for_all_elements(self.ACCORDION_MENU)[1].click()
+
+    def activation_status_settings(self):
         self.send_keys(self.NOTE_TEXT_AREA, data.qa_note)
         self.driver.execute_script("arguments[0].click()", self.presence_for_element(self.TEST_RADIO_BUTTON))
         self.presence_for_element(self.SAVE_BUTTON).click()
