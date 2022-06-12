b27efa9c0b148987244c9f9dab7792f720b8ad30
Onur Can Yöndem <99740071+onur-yondem@users.noreply.github.com>
2022-06-12 16:42:48 +0300

    Update panel_campaign_page.py

--- page/panel_campaign_page.py
+++ page/panel_campaign_page.py
@@ -55,16 +55,18 @@ class PanelCampaignPage(BaseClass):
         self.actions = ActionChains(self.driver)
         super().__init__(self.driver)
 
-    def create_campaign(self):
-        """
-        Create new campaign and edit settings
-
-        """
-
     def segment_settings(self):
+        """
+        Segment settings for new campaign
+
+        """
         self.presence_for_element(self.SAVE_BUTTON).click()
 
     def rule_settings(self):
+        """
+        Rule settings for new campaign
+
+        """
         time.sleep(5)
         self.move_to_element(self.PAGE_RULES_BUTTON)
         self.presence_for_element(self.PAGE_RULES_DROP_DOWN).click()
@@ -74,6 +76,10 @@ class PanelCampaignPage(BaseClass):
         self.presence_for_element(self.SAVE_BUTTON).click()
 
     def desing_settings(self):
+        """
+        Design settings for new campaign
+
+        """
         self.presence_for_element(self.ADD_NEW_VARIANT_BUTTON).click()
         time.sleep(20)
         self.move_to_element(self.FIRST_TEMPLATE_OVERLAY, False)
@@ -89,14 +95,26 @@ class PanelCampaignPage(BaseClass):
         self.presence_for_element(self.SAVE_BUTTON).click()
 
     def goal_settings(self):
+        """
+        Goal settings for new campaign
+
+        """
         self.presence_for_element(self.ADD_GOAL_BUTTON)
         self.presence_for_element(self.SAVE_BUTTON).click()
 
     def language_settings(self):
+        """
+        Language settings for new campaign
+
+        """
         self.presence_for_element(self.LANGUAGE_DROP_DOWN).click()
         self.presence_for_element(self.ALL_LANGUAGES_BUTTON).click()
 
     def activation_time_settings(self):
+        """
+        Activation time settings for new campaign in launch step
+
+        """
         self.presence_for_all_elements(self.END_DATE)[1].click()
         self.presence_for_element(self.YEAR_UP_BUTTON).click()
         self.presence_for_element(self.YEAR_UP_BUTTON).clear()
@@ -104,6 +122,10 @@ class PanelCampaignPage(BaseClass):
         self.presence_for_all_elements(self.WEEKEND_BUTTON)[5].click()
 
     def display_settings(self):
+        """
+        Display settings for new campaign in launch step
+
+        """
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         self.presence_for_element(self.ACCORDION_MENU).click()
         self.driver.execute_script("arguments[0].click()", self.presence_for_element(self.DISPLAY_SELECTED_DAYS_BUTTON))
@@ -117,12 +139,20 @@ class PanelCampaignPage(BaseClass):
         time.sleep(1)
 
     def advanced_settings(self):
+        """
+        Advanced settings for new campaign in launch step
+
+        """
         self.presence_for_all_elements(self.ACCORDION_MENU)[1].click()
         self.presence_for_element(self.PRIORITY_INPUT).click()
         self.presence_for_element(self.PRIORITY_TWO_BUTTON).click()
         self.presence_for_all_elements(self.ACCORDION_MENU)[1].click()
 
     def activation_status_settings(self):
+        """
+        Activation status settings for new campaign in launch step
+
+        """
         self.send_keys(self.NOTE_TEXT_AREA, data.qa_note)
         self.driver.execute_script("arguments[0].click()", self.presence_for_element(self.TEST_RADIO_BUTTON))
         self.presence_for_element(self.SAVE_BUTTON).click()
